e2941c21ccf5aa57ee0590c0afeb2df6533a9afb
franchioping <franchioping@gmail.com>
2022-05-03 18:31:20 +0100

    Updated Makefile ISA DMA implementation, should be remade later down the line if we use it for smoething other than floppy

--- A_Setup/setup-gcc-arch.sh
+++ A_Setup/setup-gcc-arch.sh
@@ -10,7 +10,7 @@ tar xf binutils-2.35.1.tar.gz
 mkdir binutils-build
 cd binutils-build
 ../binutils-2.35.1/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
-sudo make all install 2>&1  | tee make.log
+sudo make all install 2>&1 | tee make.log
 
 cd /tmp/src
 curl -O https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
@@ -20,13 +20,13 @@ cd gcc-build
 echo Configure: . . . . . . .
 ../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c++ --without-headers
 echo MAKE ALL-GCC:
-sudo make all-gcc 
+sudo make all-gcc
 echo MAKE ALL-TARGET-LIBGCC:
-sudo make all-target-libgcc 
+sudo make all-target-libgcc
 echo MAKE INSTALL-GCC:
-sudo make install-gcc 
+sudo make install-gcc
 echo MAKE INSTALL-TARGET-LIBGCC:
-sudo make install-target-libgcc 
+sudo make install-target-libgcc
 echo HERE U GO MAYBE:
 ls /usr/local/i386elfgcc/bin
 export PATH="$PATH:/usr/local/i386elfgcc/bin"
