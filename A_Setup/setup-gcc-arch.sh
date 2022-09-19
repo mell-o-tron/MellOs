037b65b7e61c859ad5925a85e9e0223187b694f7
jumps <jumpsareop@gmail.com>
2022-09-19 19:11:30 +0300

    Add error handling for setup system

--- A_Setup/setup-gcc-arch.sh
+++ A_Setup/setup-gcc-arch.sh
@@ -1,22 +1,38 @@
-sudo pacman -S nasm qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat base-devel bison flex gmp libmpc mpfr texinfo
-export PREFIX="/usr/local/i386elfgcc"
+#!/bin/sh
+
+# If any program returned non-zero value, exit.
+set -e
+
+sudo pacman -S --needed nasm qemu virt-manager virt-viewer dnsmasq \
+	vde2 bridge-utils openbsd-netcat base-devel bison flex gmp libmpc \
+	mpfr texinfo
+
+export PREFIX=/usr/local/i386elfgcc
 export TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
 
-mkdir /tmp/src
-cd /tmp/src
+tmpdir=$(mktemp)
+cd "$tmpdir"
+
 curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.gz
 tar xf binutils-2.35.1.tar.gz
+
+# Binutils
 mkdir binutils-build
 cd binutils-build
-../binutils-2.35.1/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
+../binutils-2.35.1/configure "--target=$TARGET" --enable-interwork \
+	--enable-multilib --disable-nls --disable-werror "--prefix=$PREFIX" 2>&1 |
+	tee configure.log
 sudo make all install 2>&1 | tee make.log
 
-cd /tmp/src
+cd "$tmpdir"
+
+# GCC
 curl -O https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
 tar xf gcc-10.2.0.tar.gz
 mkdir gcc-build
 cd gcc-build
+
 echo Configure: . . . . . . .
 ../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c,c++ --without-headers
 echo MAKE ALL-GCC:
@@ -28,5 +44,6 @@ sudo make install-gcc
 echo MAKE INSTALL-TARGET-LIBGCC:
 sudo make install-target-libgcc
 echo HERE U GO MAYBE:
+
 ls /usr/local/i386elfgcc/bin
 export PATH="$PATH:/usr/local/i386elfgcc/bin"
--- A_Setup/setup-gcc-debian.sh
+++ A_Setup/setup-gcc-debian.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+# If any program returned non-zero value, exit.
+set -e
+
 # nasm and qemu
 sudo apt install nasm
 sudo apt install qemu
@@ -18,22 +23,29 @@ export PREFIX="/usr/local/i386elfgcc"
 export TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
 
-mkdir /tmp/src
-cd /tmp/src
+tmpdir=$(mktemp)
+cd "$tmpdir"
+
 curl -O http://ftp.gnu.org/gnu/binutils/binutils-2.35.1.tar.gz
 tar xf binutils-2.35.1.tar.gz
+
 mkdir binutils-build
 cd binutils-build
-../binutils-2.35.1/configure --target=$TARGET --enable-interwork --enable-multilib --disable-nls --disable-werror --prefix=$PREFIX 2>&1 | tee configure.log
+../binutils-2.35.1/configure "--target=$TARGET" --enable-interwork \
+	--enable-multilib --disable-nls --disable-werror "--prefix=$PREFIX" 2>&1 |
+	tee configure.log
 sudo make all install 2>&1 | tee make.log
 
-cd /tmp/src
+cd "$tmpdir"
+
 curl -O https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz
 tar xf gcc-10.2.0.tar.gz
+
 mkdir gcc-build
 cd gcc-build
 echo Configure: . . . . . . .
-../gcc-10.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --disable-libssp --enable-language=c,c++ --without-headers
+../gcc-10.2.0/configure "--target=$TARGET" "--prefix=$PREFIX" --disable-nls \
+	--disable-libssp --enable-language=c,c++ --without-headers
 echo MAKE ALL-GCC:
 sudo make all-gcc
 echo MAKE ALL-TARGET-LIBGCC:
@@ -43,5 +55,6 @@ sudo make install-gcc
 echo MAKE INSTALL-TARGET-LIBGCC:
 sudo make install-target-libgcc
 echo HERE U GO MAYBE:
+
 ls /usr/local/i386elfgcc/bin
 export PATH="$PATH:/usr/local/i386elfgcc/bin"
