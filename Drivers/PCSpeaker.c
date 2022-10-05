fa3be49f1ea21b3928502d5ef191b5947d7e11bb
King Undead <70785950+KingUndeadCodes@users.noreply.github.com>
2022-10-05 18:50:42 -0500

    Added support for the PC Speaker.
    Support for the PC Speaker.

---
+++ Drivers/PCSpeaker.c
@@ -0,0 +1,34 @@
+// Written by KingUndeadCodes (https://www.github.com/KingUndeadCodes/)
+// Check out my OS (https://www.github.com/KingUndeadCodes/Pumpkin-OS)
+
+#include "../CPU/Timer/timer.h"
+
+static void PlaySound(unsigned int nFrequence) {
+    unsigned char x;
+    unsigned char y;
+    x = 1193180 / nFrequence;
+    outb(0x43, 0xb6);
+    outb(0x42, (unsigned char)(x));
+    outb(0x42, (unsigned char)(x >> 8));
+    y = inb(0x61);
+    if (y != (y | 3)) {
+ 	    outb(0x61, y | 3);
+    }
+}
+
+static void Quiet() {
+    unsigned char x = inb(0x61) & 0xFC;
+    outb(0x61, x);
+}
+
+void beep(unsigned int freq, unsigned int time) {
+    if (!freq) freq = 950;
+    if (!time) time = 18;
+    if (time == 0) {
+        print("beep warning: 'time' played is zero.");
+        return;
+    }
+    PlaySound(freq);
+    timer_wait(time);
+    Quiet();
+}
---
+++ Drivers/PCSpeaker.h
@@ -0,0 +1,8 @@
+#ifndef _PC_SPEAKER
+#define _PC_SPEAKER
+
+static void PlaySound(unsigned int nFrequence);
+static void Quiet();
+void beep(unsigned int freq, unsigned int time = 18);
+
+#endif
