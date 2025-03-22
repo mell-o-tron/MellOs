44e8f71bd1dac583e24f1d273aa0c236cec3d0b3
Alex23087 <alessandro.scala8@gmail.com>
2025-03-22 23:43:12 +0100

    Add kprint hex and dec

--- drivers/vga_text.h
+++ drivers/vga_text.h
@@ -9,6 +9,8 @@ void clear_line_col(uint32_t line, Colour col);
 void kprint_col(const char* s, Colour col);
 void kprint(const char* s);
 void kprint_char (char c, bool caps);
+void kprint_dec(uint32_t n);
+void kprint_hex(uint32_t n);
 
 void move_cursor_LR(int i);
 void move_cursor_UD(int i);
