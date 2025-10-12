da56c32ddbac9548c2f32176dddb0e24cb97b0b6
Alessandro Scala <44084921+Alex23087@users.noreply.github.com>
2025-10-12 10:12:52 +0100

    Remove virt-manager and virt-viewer from arch setup script. Add missing cmake dependency

--- A_Setup/setup-gcc-arch.sh
+++ A_Setup/setup-gcc-arch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo pacman -S nasm qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat base-devel bison flex gmp libmpc mpfr texinfo mtools
+sudo pacman -S nasm qemu cmake dnsmasq vde2 bridge-utils openbsd-netcat base-devel bison flex gmp libmpc mpfr texinfo mtools
 export PREFIX="/usr/local/i386elfgcc"
 export TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
