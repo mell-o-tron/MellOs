22604e6e97530e969ea518509dad9ea2f2f18ab8
Matias <kmatias@kmatias.dev>
2025-12-05 16:54:07 +0200

    Fixed gitignore.

---
+++ drivers/include/pc_speaker.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifdef AUDIO_ENABLED
+
+static void PlaySound(unsigned int nFrequence);
+static void Quiet();
+void beep(unsigned int freq, unsigned int time);
+
+#endif
---
+++ drivers/include/rtc.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "stdint.h"
+
+void print_time(const char* s);
+uint32_t posix_time();
